0c27232e02902117be14804bffe52120ee2daa98
JaspreetSinghGill <jaspreetsg06@gmail.com>
2019-12-30 14:27:55 +0530

    ADDED: flipped a coin and displayed head or tail as winner

---
+++ ucOneFlipCoin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+#FLIP COIN COMBINATION: USE CASE 1 -> FLIP A COIN AND DISPLAY HEAD OR TAIL AS WINNER.
+
+getFlipCoinResult () {
+	result=$(( RANDOM%2 ));
+
+	if [ $result -eq 1 ]
+	then
+		echo "HEAD";
+	else
+		echo "TAIL";
+	fi;
+}
+
+flipCoinMain () {
+	getFlipCoinResult;
+}
+
+flipCoinMain;
